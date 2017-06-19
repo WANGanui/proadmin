fd280fb2339e59894d9d331b6a719029abf7bb8f
347381524@qq.com <tc147258>
2017-06-19 14:50:34 +0800

    项目列表

--- src/main/java/com/hrg/module/project/ProjectController.java
+++ src/main/java/com/hrg/module/project/ProjectController.java
@@ -1,7 +1,35 @@
 package com.hrg.module.project;
 
+import com.hrg.model.Project;
+import com.hrg.model.ProjectCriteria;
+import com.hrg.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
 /**
  * Created by 82705 on 2017/6/14.
  */
+@Controller
 public class ProjectController {
+
+    @Autowired
+    ProjectService projectService;
+
+    @RequestMapping(value = "/projectList")
+    public ModelAndView selectProject(){
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.setViewName("project_list");
+        try {
+            ProjectCriteria projectCriteria=new ProjectCriteria();
+            List<Project> projectList= projectService.selectList(projectCriteria);
+            modelAndView.addObject("projectList",projectList);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return modelAndView;
+    }
 }
