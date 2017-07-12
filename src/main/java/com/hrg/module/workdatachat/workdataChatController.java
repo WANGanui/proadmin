e4f21c832661e1bb96b3b79cc23d663dcad7940c
347381524@qq.com <tc147258>
2017-07-12 10:23:04 +0800

    日志

--- src/main/java/com/hrg/module/workdatachat/workdataChatController.java
+++ src/main/java/com/hrg/module/workdatachat/workdataChatController.java
@@ -25,9 +25,19 @@ public class workdataChatController {
     WorkDataService workDataService;
 
     @RequestMapping(value = "/workDataChatHtm")
-    public String workDataChatHtm(HttpServletRequest request,String dataId){
+    public String workDataChatHtm(HttpServletRequest request,String dataId,HttpSession session){
         //request.setAttribute("dataId",dataId);
         try {
+            Worker worker=(Worker) session.getAttribute("worker");
+           // WorkdataChat record=new WorkdataChat();
+            Map record= new HashMap();
+            record.put("isread","1");
+            record.put("chatid",worker.getDataid());
+            record.put("workdataid",dataId);
+            /* WorkdataChatCriteria example=new WorkdataChatCriteria();
+            example.setChatid(worker.getDataid());
+            example.setWorkdataid(dataId);*/
+            workDataService.updateByExampleSelective(record);
             WorkdataChatCriteria workdataChatCriteria=new WorkdataChatCriteria();
             workdataChatCriteria.setWorkdataid(dataId);
             workdataChatCriteria.setOrderByClause("dataid ASC");
@@ -49,6 +59,7 @@ public class workdataChatController {
             workdataChat.setChatname(worker.getName());//评论人姓名
             workdataChat.setContext(map.get("context").toString());//回复内容
             workdataChat.setWorkdataid(map.get("workDataId").toString());//日志ID
+            workdataChat.setIsread("0");//添加时未读状态
             workDataService.insert(workdataChat);
             result.put("success",true);
         }catch (Exception e){
