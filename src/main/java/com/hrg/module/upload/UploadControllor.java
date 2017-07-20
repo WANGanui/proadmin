b6815655418c17fbcf90f97f339628393c843838
wanganui <827052997@qq.com>
2017-07-20 17:32:09 +0800

    修改bug

---
+++ src/main/java/com/hrg/module/upload/UploadControllor.java
@@ -0,0 +1,127 @@
+package com.hrg.module.upload;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadBase;
+import org.apache.commons.fileupload.ProgressListener;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+/**
+ * Created by 82705 on 2017/7/20.
+ */
+@Controller
+public class UploadControllor {
+
+    @RequestMapping("/uploadfile")
+    public @ResponseBody Object uploadfile(HttpServletRequest request, HttpServletResponse response) {
+        Map map = new HashMap();
+        String savePath = request.getSession().getServletContext().getRealPath("/WEB-INF/upload");
+        String tempPath = request.getSession().getServletContext().getRealPath("/WEB-INF/temp");
+        File tmpFile = new File(tempPath);
+        if (!tmpFile.exists()) {
+            tmpFile.mkdir();
+        }
+        try {
+            DiskFileItemFactory factory = new DiskFileItemFactory();
+            factory.setSizeThreshold(1024 * 100);//设置缓冲区的大小为100KB，如果不指定，那么缓冲区的大小默认是10KB
+            factory.setRepository(tmpFile);
+            ServletFileUpload upload = new ServletFileUpload(factory);
+            upload.setProgressListener(new ProgressListener() {
+                public void update(long pBytesRead, long pContentLength, int arg2) {
+                    System.out.println("文件大小为：" + pContentLength + ",当前已处理：" + pBytesRead);
+                }
+            });
+            upload.setHeaderEncoding("UTF-8");
+            if (!ServletFileUpload.isMultipartContent(request)) {
+                return null;
+            }
+            upload.setFileSizeMax(1024 * 1024);
+            upload.setSizeMax(1024 * 1024 * 10);
+            List<FileItem> list = upload.parseRequest(request);
+            for (FileItem item : list) {
+                if (item.isFormField()) {
+                    String name = item.getFieldName();
+                    String value = item.getString("UTF-8");
+                    System.out.println(name + "=" + value);
+                } else {
+                    String filename = item.getName();
+                    System.out.println(filename);
+                    if (filename == null || filename.trim().equals("")) {
+                        continue;
+                    }
+                    filename = filename.substring(filename.lastIndexOf("\\") + 1);
+                    String fileExtName = filename.substring(filename.lastIndexOf(".") + 1);
+                    System.out.println("上传的文件的扩展名是：" + fileExtName);
+                    InputStream in = item.getInputStream();
+                    String saveFilename = makeFileName(filename);
+                    String realSavePath = makePath(saveFilename, savePath);
+                    FileOutputStream out = new FileOutputStream(realSavePath + "\\" + saveFilename);
+                    byte buffer[] = new byte[1024];
+                    int len = 0;
+                    while ((len = in.read(buffer)) > 0) {
+                        out.write(buffer, 0, len);
+                    }
+                    in.close();
+                    out.close();
+                    map.put("filename",saveFilename);
+                }
+            }
+        } catch (FileUploadBase.FileSizeLimitExceededException e) {
+            e.printStackTrace();
+            map.put("success",false);
+        } catch (FileUploadBase.SizeLimitExceededException e) {
+            e.printStackTrace();
+            map.put("success",false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            map.put("success",false);
+        }
+        return map;
+    }
+
+    /**
+     * @Method: makeFileName
+     * @Description: 生成上传文件的文件名，文件名以：uuid+"_"+文件的原始名称
+     * @Anthor:孤傲苍狼
+     * @param filename 文件的原始名称
+     * @return uuid+"_"+文件的原始名称
+     */
+    private String makeFileName(String filename){  //2.jpg
+        //为防止文件覆盖的现象发生，要为上传文件产生一个唯一的文件名
+        return UUID.randomUUID().toString() + "_" + filename;
+    }
+
+    /**
+     * 为防止一个目录下面出现太多文件，要使用hash算法打散存储
+     * @Method: makePath
+     * @Description:
+     * @Anthor:孤傲苍狼
+     *
+     * @param filename 文件名，要根据文件名生成存储目录
+     * @param savePath 文件存储路径
+     * @return 新的存储目录
+     */
+    private String makePath(String filename,String savePath){
+        int hashcode = filename.hashCode();
+        int dir1 = hashcode&0xf;  //0--15
+        int dir2 = (hashcode&0xf0)>>4;  //0-15
+        String dir = savePath + "\\" + dir1 + "\\" + dir2;  //upload\2\3  upload\3\5
+        File file = new File(dir);
+        if(!file.exists()){
+            file.mkdirs();
+        }
+        return dir;
+    }
+}
